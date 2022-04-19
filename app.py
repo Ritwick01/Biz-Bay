fd3c5806063bca66517c3d6748fb9d0021bd6412
Ritwick01 <ritwick20461@iiitd.ac.in>
2022-04-20 01:22:05 +0530

    Added Registration Page

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+
 app = Flask(__name__)
 import mysql.connector
 mydb=mysql.connector.connect(host="localhost",user="root",passwd="password", database="testing")
 mycursor = mydb.cursor()
+app.config.update(dict(
+    SECRET_KEY="powerful secretkey",
+    WTF_CSRF_SECRET_KEY="a csrf secret key"
+))
 
 @app.route('/')
 @app.route('/home')
@@ -14,3 +21,17 @@ def market_page():
     mycursor.execute("SELECT * FROM Ads")
     myresult = mycursor.fetchall()
     return render_template('market.html', myresult=myresult)
+
+class RegisterForm(FlaskForm):
+    username = StringField(label='Name:')
+    age = StringField(label='Age:')
+    email_address = StringField(label='Email Address:')
+    password1 = PasswordField(label='Password:')
+    password2 = PasswordField(label='Confirm Password:')
+    phoneno = StringField(label='Phone Number:')
+    submit = SubmitField(label='Create Account')
+
+@app.route('/register')
+def register_page():
+    form = RegisterForm()
+    return render_template('register.html', form=form)
